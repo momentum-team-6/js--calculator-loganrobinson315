1cb80a8003067ffb868fda6b7fc3015527f2ffd3
Logan Robinson <loganrobinson315@gmail.com>
2020-11-13 10:34:54 -0500

    calc-first-attempt

---
+++ main.js
@@ -0,0 +1,21 @@
+const calculator = document.querySelector('.calculator-container')
+// const operationButtons = document.querySelectorAll('[data-operation]')
+// const equalsButton = document.querySelector('[data-equals]')
+
+
+calculator.addEventListener('click', function (event) {
+    console.log(event)
+    console.log(event.target)
+    console.log(event.target.innerText)
+    const display = document.querySelector('#output')
+    display.innerHTML = event.target.innerText
+    
+})
+// console.log('hello')
+// 
+window.alert('Hello, welcome to my calculator!')
+
+document.querySelector('.clear').addEventListener('click', function (event) {
+    const display = document.querySelector('#output')
+    display.innerHTML = event.target.innerHTML= ""
+})
